561e88f32412689a5a8f9b9b69b8a6dc5f8e8202
tolbek <ahmetfiratcs@gmail.com>
2024-11-26 16:25:46 +0300

    comments added explaining recursive calls

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@ void ToH(int n, int A, int B, int C)
 {
   if (n > 0)
    {
+    
+    // Disks will be moved from tower A to tower C using tower B
+
     ToH(n-1,A,C,B);
+
+    // Prints necessary steps in following format (disk is being moved from,disk is being moved to)
+
     std::cout << "(" << A << "," << C << ")" << "\n";
+
+   // Disks will be moved from tower B to tower A using tower C
+
     ToH(n-1,B,A,C);
    }
 }
